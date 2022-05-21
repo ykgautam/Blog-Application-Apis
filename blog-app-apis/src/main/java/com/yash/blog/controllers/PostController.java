8113f9bf6abe8c953f74c9ec2a2a09a46a5754fd
Yash <yk.gautam97@gmail.com>
2022-05-21 12:59:14 +0530

    added feature of uploading post image and downloadImage

--- blog-app-apis/src/main/java/com/yash/blog/controllers/PostController.java
+++ blog-app-apis/src/main/java/com/yash/blog/controllers/PostController.java
@@ -1,11 +1,17 @@
 package com.yash.blog.controllers;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.hibernate.engine.jdbc.StreamUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +22,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.yash.blog.config.AppConstants;
 import com.yash.blog.payloads.ApiResponse;
 import com.yash.blog.payloads.PostDto;
 import com.yash.blog.payloads.PostResponse;
+import com.yash.blog.services.FileService;
 import com.yash.blog.services.PostService;
 
 @RestController
@@ -29,6 +37,12 @@ public class PostController {
 
 	@Autowired
 	PostService postService;
+
+	@Autowired
+	FileService fileService;
+
+	@Value("${project.image}")
+	private String path;
 
 	// create
 	@PostMapping("/user/{userId}/category/{categoryId}/posts")
@@ -56,12 +70,11 @@ public class PostController {
 	// get all posts
 	@GetMapping("/posts")
 	public ResponseEntity<PostResponse> getAllPosts(
-			@RequestParam(value = "pageNumber" , defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
-			@RequestParam(value = "pageSize" , defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
-			@RequestParam(value = "sortBy" , defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
-			@RequestParam(value = "sortDir" , defaultValue = AppConstants.SORT_DIR, required = false) String sortDir
-			) {
-		 PostResponse postResponse = this.postService.getAllPosts(pageNumber,pageSize,sortBy,sortDir);
+			@RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+			@RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+			@RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+			@RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir) {
+		PostResponse postResponse = this.postService.getAllPosts(pageNumber, pageSize, sortBy, sortDir);
 		return ResponseEntity.ok(postResponse);
 	}
 
@@ -79,19 +92,42 @@ public class PostController {
 		PostDto postDto = this.postService.getPostById(postId);
 		return new ResponseEntity<PostDto>(postDto, HttpStatus.OK);
 	}
-	
+
 	// get single post
-		@PutMapping("/post/{postId}")
-		public ResponseEntity<PostDto> updatePostById(@RequestBody PostDto postDto,@PathVariable Integer postId) {
-			PostDto updatedPOst = this.postService.updatePost(postDto,postId);
-			return new ResponseEntity<PostDto>(updatedPOst, HttpStatus.OK);
-		}
-	
-		// search
-		@GetMapping("/posts/search/{keywords}")
-		public ResponseEntity<List<PostDto>> searchPostByTitle(@PathVariable String keywords){
-			List<PostDto> searchPosts = this.postService.searchPosts(keywords);
-			
-			return new ResponseEntity<List<PostDto>>(searchPosts,HttpStatus.OK);
-		}
+	@PutMapping("/post/{postId}")
+	public ResponseEntity<PostDto> updatePostById(@RequestBody PostDto postDto, @PathVariable Integer postId) {
+		PostDto updatedPOst = this.postService.updatePost(postDto, postId);
+		return new ResponseEntity<PostDto>(updatedPOst, HttpStatus.OK);
+	}
+
+	// search
+	@GetMapping("/posts/search/{keywords}")
+	public ResponseEntity<List<PostDto>> searchPostByTitle(@PathVariable String keywords) {
+		List<PostDto> searchPosts = this.postService.searchPosts(keywords);
+
+		return new ResponseEntity<List<PostDto>>(searchPosts, HttpStatus.OK);
+	}
+
+	// post image upload
+	@PostMapping("/post/image/upload/{postId}")
+	public ResponseEntity<PostDto> uploadPostImage(@PathVariable("postId") Integer postId,
+			@RequestParam("image") MultipartFile image) throws IOException {
+
+		PostDto postDto = this.postService.getPostById(postId);
+		String fileName = this.fileService.uploadImage(path, image);
+		postDto.setImageName(fileName);
+		PostDto updatedPost = this.postService.updatePost(postDto, postId);
+
+		return new ResponseEntity<PostDto>(updatedPost, HttpStatus.OK);
+
+	}
+
+	// method to serve file
+	@GetMapping(value = "/post/image/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+	public void downloadImage(@PathVariable("imageName") String imageName, HttpServletResponse response)
+			throws IOException {
+		InputStream resource = this.fileService.getResource(path, imageName);
+		response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+		StreamUtils.copy(resource, response.getOutputStream());
+	}
 }
